cd2bb1a2f1d8162ddb3624fb6159bd951fa1f800
Nicholas Huynh <nickhuynhq@gmail.com>
2022-08-09 23:23:08 -0400

    Add elements when search results are undefined

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,90 +28,100 @@ function handleButton(event){
             let movieRating = movieInfo.imdbRating;
             let movieId = movieInfo.imdbID;
 
-
             let containerEl = document.querySelector(".movie");
             containerEl.innerHTML="";
 
-            // Left Section
-            let movieLeft = document.createElement("div");
-            movieLeft.classList.add("movie__left");
-            containerEl.appendChild(movieLeft);
+            if(movieTitle === undefined){
+                let errorEl = document.createElement("img");
+                errorEl.classList.add("movie__error");
+                errorEl.setAttribute("src", "../images/invalid-title.png");
+                containerEl.appendChild(errorEl);
 
-            let titleEl = document.createElement("h2");
-            titleEl.classList.add("movie__title--mobile");
-            titleEl.textContent = movieTitle;
-            movieLeft.appendChild(titleEl);
+                let errorMsgEl = document.createElement("h2");
+                errorMsgEl.classList.add("movie__error-message");
+                errorMsgEl.textContent = `Please enter a valid Movie Title`;
+                containerEl.appendChild(errorMsgEl);
 
-            let yearEl = document.createElement("h2");
-            yearEl.classList.add("movie__year--mobile");
-            yearEl.textContent = `(${movieYear})`;
-            movieLeft.appendChild(yearEl);
+            } else {
 
-            let posterAnchor = document.createElement("a");
-            posterAnchor.classList.add("movie__anchor");
-            posterAnchor.setAttribute("href", `https://www.imdb.com/title/${movieId}/`);
-            posterAnchor.setAttribute("target", "_blank");
-            movieLeft.appendChild(posterAnchor);
+                 // Left Section
+                let movieLeft = document.createElement("div");
+                movieLeft.classList.add("movie__left");
+                containerEl.appendChild(movieLeft);
 
-            let posterEl = document.createElement("img");
-            posterEl.classList.add("movie__poster");
-            posterEl.setAttribute("src", moviePoster);
-            posterAnchor.appendChild(posterEl);
+                let titleEl = document.createElement("h2");
+                titleEl.classList.add("movie__title--mobile");
+                titleEl.textContent = movieTitle;
+                movieLeft.appendChild(titleEl);
 
-            // Right Section
-            let movieRight = document.createElement("div");
-            movieRight.classList.add("movie__right");
-            containerEl.appendChild(movieRight);
+                let yearEl = document.createElement("h2");
+                yearEl.classList.add("movie__year--mobile");
+                yearEl.textContent = `(${movieYear})`;
+                movieLeft.appendChild(yearEl);
 
-            let movieRightTop = document.createElement("div");
-            movieRightTop.classList.add("movie__right-top");
-            movieRight.appendChild(movieRightTop);
+                let posterAnchor = document.createElement("a");
+                posterAnchor.classList.add("movie__anchor");
+                posterAnchor.setAttribute("href", `https://www.imdb.com/title/${movieId}/`);
+                posterAnchor.setAttribute("target", "_blank");
+                movieLeft.appendChild(posterAnchor);
 
-            let titleEl2 = document.createElement("h2");
-            titleEl2.classList.add("movie__title--not-mobile");
-            titleEl2.textContent = movieTitle;
-            movieRightTop.appendChild(titleEl2);
+                let posterEl = document.createElement("img");
+                posterEl.classList.add("movie__poster");
+                posterEl.setAttribute("src", moviePoster);
+                posterAnchor.appendChild(posterEl);
 
-            let yearEl2 = document.createElement("h2");
-            yearEl2.classList.add("movie__year--not-mobile");
-            yearEl2.textContent = `(${movieYear})`;
-            movieRightTop.appendChild(yearEl2);
+                // Right Section
+                let movieRight = document.createElement("div");
+                movieRight.classList.add("movie__right");
+                containerEl.appendChild(movieRight);
 
-            let movieRightMid = document.createElement("div");
-            movieRightMid.classList.add("movie__right-middle");
-            movieRight.appendChild(movieRightMid);
+                let movieRightTop = document.createElement("div");
+                movieRightTop.classList.add("movie__right-top");
+                movieRight.appendChild(movieRightTop);
 
-            let genreEl = document.createElement("h3")
-            genreEl.classList.add("movie__plot");
-            genreEl.textContent = movieGenre;
-            movieRightMid.appendChild(genreEl);
+                let titleEl2 = document.createElement("h2");
+                titleEl2.classList.add("movie__title--not-mobile");
+                titleEl2.textContent = movieTitle;
+                movieRightTop.appendChild(titleEl2);
 
-            let ratingContainer = document.createElement("div");
-            ratingContainer.classList.add("movie__rating-container");
-            ratingContainer.setAttribute("src", "../images/gold-star.svg.png");
-            movieRightMid.appendChild(ratingContainer);
+                let yearEl2 = document.createElement("h2");
+                yearEl2.classList.add("movie__year--not-mobile");
+                yearEl2.textContent = `(${movieYear})`;
+                movieRightTop.appendChild(yearEl2);
 
-            let starEl = document.createElement("img");
-            starEl.classList.add("movie__star");
-            starEl.setAttribute("src", "../images/gold-star.svg.png");
-            ratingContainer.appendChild(starEl);
+                let movieRightMid = document.createElement("div");
+                movieRightMid.classList.add("movie__right-middle");
+                movieRight.appendChild(movieRightMid);
 
-            let ratingEl = document.createElement("p")
-            starEl.classList.add("movie__rating");
-            ratingEl.textContent = movieRating;
-            ratingContainer.appendChild(ratingEl);
+                let genreEl = document.createElement("h3")
+                genreEl.classList.add("movie__plot");
+                genreEl.textContent = movieGenre;
+                movieRightMid.appendChild(genreEl);
 
-    
-            let movieInfoEl = document.createElement("div");
-            movieInfoEl.classList.add("movie__info");
-            movieRight.appendChild(movieInfoEl);
+                let ratingContainer = document.createElement("div");
+                ratingContainer.classList.add("movie__rating-container");
+                ratingContainer.setAttribute("src", "../images/gold-star.svg.png");
+                movieRightMid.appendChild(ratingContainer);
 
-            let plotEl = document.createElement("p")
-            plotEl.classList.add("movie__plot");
-            plotEl.textContent = moviePlot;
-            movieInfoEl.appendChild(plotEl);
-        
-        
+                let starEl = document.createElement("img");
+                starEl.classList.add("movie__star");
+                starEl.setAttribute("src", "../images/gold-star.svg.png");
+                ratingContainer.appendChild(starEl);
+
+                let ratingEl = document.createElement("p")
+                starEl.classList.add("movie__rating");
+                ratingEl.textContent = movieRating;
+                ratingContainer.appendChild(ratingEl);
+
+                let movieInfoEl = document.createElement("div");
+                movieInfoEl.classList.add("movie__info");
+                movieRight.appendChild(movieInfoEl);
+
+                let plotEl = document.createElement("p")
+                plotEl.classList.add("movie__plot");
+                plotEl.textContent = moviePlot;
+                movieInfoEl.appendChild(plotEl);
+            };
         }).catch(error => {
             console.log(error);
         });
